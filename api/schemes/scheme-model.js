d7860f805870910c9c2255a123adcc8587dbd0b1
BrianReisman <brianreisman@protonmail.com>
2021-03-30 22:55:50 -0400

    improved findById. MPV met

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -28,63 +28,29 @@ function find() {
 // !!
 async function findById(scheme_id) {
   try {
-    const array = await db("schemes")
+    const steps = await db("schemes")
       .leftJoin("steps", "steps.scheme_id", "schemes.scheme_id")
-      .select(
-        "schemes.scheme_name",
-        "steps.step_id",
-        "steps.step_number",
-        "steps.instructions",
-        "steps.scheme_id"
-      )
+      .select("steps.step_number", "steps.instructions", "steps.step_id")
       .where({ "steps.scheme_id": scheme_id })
       .orderBy("steps.step_number");
-    if (array.length === 0) {
-      return []
-    } else {
-      console.log("inside findById -model", array.length);
+
+    if (steps.length === 0) {
+      return {
+        scheme_id: scheme_id,
+        steps: [],
+      };    } else {
+      return {
+        scheme_id: scheme_id,
+        // scheme_name: "World Domination",
+        steps,
+      };
     }
   } catch (err) {
     console.log(err, "inside error");
   }
 
   /*
-    1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
-
-      SELECT
-          sc.scheme_name,
-          st.*
-      FROM schemes as sc
-      LEFT JOIN steps as st
-          ON sc.scheme_id = st.scheme_id
-      WHERE sc.scheme_id = 1
-      ORDER BY st.step_number ASC;
-
-    // 2B- When you have a grasp on the query go ahead and build it in Knex
-    // making it parametric: instead of a literal `1` you should use `scheme_id`.
-
-    // 3B- Test in Postman and see that the resulting data does not look like a scheme,
-    // but more like an array of steps each including scheme information:
-
-      // [
-      //   {
-      //     "scheme_id": 1,
-      //     "scheme_name": "World Domination",
-      //     "step_id": 2,
-      //     "step_number": 1,
-      //     "instructions": "solve prime number theory"
-      //   },
-      //   {
-      //     "scheme_id": 1,
-      //     "scheme_name": "World Domination",
-      //     "step_id": 1,
-      //     "step_number": 2,
-      //     "instructions": "crack cyber security"
-      //   },
-        // etc
-      // ]
-
-    4B- Using the array obtained and vanilla JavaScript, create an object with the structure below, for the case _when steps exist_ for a given `scheme_id`:
+      4B- Using the array obtained and vanilla JavaScript, create an object with the structure below, for the case _when steps exist_ for a given `scheme_id`:
 
       {
         "scheme_id": 1,
@@ -111,6 +77,42 @@ async function findById(scheme_id) {
         "scheme_name": "Have Fun!",
         "steps": []
       }
+
+    // 1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
+
+    //   SELECT
+    //       sc.scheme_name,
+    //       st.*
+    //   FROM schemes as sc
+    //   LEFT JOIN steps as st
+    //       ON sc.scheme_id = st.scheme_id
+    //   WHERE sc.scheme_id = 1
+    //   ORDER BY st.step_number ASC;
+
+    // 2B- When you have a grasp on the query go ahead and build it in Knex
+    // making it parametric: instead of a literal `1` you should use `scheme_id`.
+
+    // 3B- Test in Postman and see that the resulting data does not look like a scheme,
+    // but more like an array of steps each including scheme information:
+
+      // [
+      //   {
+      //     "scheme_id": 1,
+      //     "scheme_name": "World Domination",
+      //     "step_id": 2,
+      //     "step_number": 1,
+      //     "instructions": "solve prime number theory"
+      //   },
+      //   {
+      //     "scheme_id": 1,
+      //     "scheme_name": "World Domination",
+      //     "step_id": 1,
+      //     "step_number": 2,
+      //     "instructions": "crack cyber security"
+      //   },
+        // etc
+      // ]
+
   */
 }
 
